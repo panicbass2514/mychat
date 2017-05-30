675928dc6d6a452924bfab5c38fd368cbf1a6b92
Paul Calinao <panicbass2514@gmail.com>
2017-05-30 16:54:47 +0800

    MyChat

---
+++ classes/ChatBase.class.php
@@ -0,0 +1,17 @@
+<?php 
+
+// This is the base class, used by Chatline and ChatUser
+
+class ChatBase {
+
+	// This constructor is used by all the chat classes:
+
+	public function __construct(array $options) {
+
+		foreach ($options as $k => $v) {
+			if (isset($this->$k)) {
+				$this->$k = $v;
+			}
+		}
+	}
+}
